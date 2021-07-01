c1dfc208216d7b3e740a9d263baab8661b5bb62a
markk10 <mark.erhardt@hotmail.com>
2021-07-01 13:07:56 +0200

    sadfgdg

--- FindYOURCar/src/Test/TestHello.java
+++ FindYOURCar/src/Test/TestHello.java
@@ -4,7 +4,7 @@ public class TestHello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hallo Mark");
+		System.out.println("Hallo");
 	}
 
 }
