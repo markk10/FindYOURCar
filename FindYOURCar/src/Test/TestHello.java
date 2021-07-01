c4029ecbed6d687989c657938cdb63c253fc42c6
Alfred <User@Alik-Laptop.fritz.box>
2021-07-01 13:09:14 +0200

    Test wiegehts

--- FindYOURCar/src/Test/TestHello.java
+++ FindYOURCar/src/Test/TestHello.java
@@ -5,6 +5,7 @@ public class TestHello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hallo Mark");
+		System.out.println("Wie gehts");
 	}
 
 }
